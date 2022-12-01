b070a0157c87ae9e2a79b9554a2b68cb1f526e26
Benjamin Stepp <me@benstepp.com>
2022-12-01 10:09:06 -0600

    Day One Puzzle Two

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -2,33 +2,44 @@ mod elf;
 mod fruit;
 
 use crate::elf::Elf;
+use std::collections::BinaryHeap;
 use std::str::Split;
+
+struct Result {
+    maximum: i32,
+    top_three: i32,
+}
 
 fn main() {
     let input = include_str!("../fixtures/input");
-    day_one(input);
+    let result = day_one(input);
+    println!("Maximum: {}", result.maximum);
+    println!("Top Three: {}", result.top_three);
 }
 
-fn day_one(input: &str) -> i32 {
-    let (_elves, max) = parse_elves(input);
-    println!("{:?}", max);
-    max
-}
-
-fn parse_elves(input: &str) -> (Vec<Elf>, i32) {
+fn day_one(input: &str) -> Result {
     let elf_inputs: Split<&str> = input.split("\n\n");
     let mut elves = Vec::new();
-    let mut max: i32 = 0;
+    let mut max_heap: BinaryHeap<i32> = BinaryHeap::new();
 
     for elf_input in elf_inputs {
         let elf: Elf = Elf::new(elf_input);
-        if elf.calories > max {
-            max = elf.calories;
-        }
+        max_heap.push(elf.calories);
         elves.push(elf);
     }
 
-    (elves, max)
+    let maximum: i32 = *max_heap.peek().unwrap();
+
+    let top_three: i32 = {
+        let mut result: i32 = 0;
+        for _ in 0..3 {
+            let val = max_heap.pop().unwrap();
+            result += val;
+        }
+        result
+    };
+
+    Result { maximum, top_three }
 }
 
 #[cfg(test)]
@@ -36,9 +47,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_input_returns_24000() {
+    fn test_input_returns_24000_maximum() {
         let input: &str = include_str!("../fixtures/test_input");
-        let result: i32 = day_one(input);
-        assert_eq!(result, 24000);
+        let result: Result = day_one(input);
+        assert_eq!(result.maximum, 24000);
+    }
+
+    #[test]
+    fn test_input_returns_45000_top_three() {
+        let input: &str = include_str!("../fixtures/test_input");
+        let result: Result = day_one(input);
+        assert_eq!(result.top_three, 45000);
     }
 }
