7175f5b00f8818000d2d2c99f2d03ec7d001f6cf
Benjamin Stepp <me@benstepp.com>
2022-12-11 17:26:43 -0600

    Day 9 Puzzle 2

--- days/day_9/src/main.rs
+++ days/day_9/src/main.rs
@@ -5,20 +5,26 @@ use crate::rope_bridge::RopeBridge;
 
 struct Result {
     unique_tail_coords: i32,
+    longer: i32,
 }
 
 fn main() {
     let input: &str = include_str!("../fixtures/input");
     let result: Result = day_nine(input);
     println!("Unique Tail Coords: {}", result.unique_tail_coords);
+    println!("Unique Tail Coords: {}", result.longer);
 }
 
 fn day_nine(input: &str) -> Result {
     let mut rope_bridge = RopeBridge::new();
     rope_bridge.add_steps(input);
 
-    let unique_tail_coords: i32 = rope_bridge.unique_tail_locations();
-    Result { unique_tail_coords }
+    let unique_tail_coords: i32 = rope_bridge.unique_tail_locations(1);
+    let longer: i32 = rope_bridge.unique_tail_locations(9);
+    Result {
+        unique_tail_coords,
+        longer,
+    }
 }
 
 #[cfg(test)]
@@ -30,5 +36,13 @@ mod tests {
         let input: &str = include_str!("../fixtures/test_input");
         let result: Result = day_nine(input);
         assert_eq!(result.unique_tail_coords, 13);
+        assert_eq!(result.longer, 1);
+    }
+
+    #[test]
+    fn bigger_test_input_works() {
+        let input: &str = include_str!("../fixtures/test_input_2");
+        let result: Result = day_nine(input);
+        assert_eq!(result.longer, 36);
     }
 }
--- days/day_9/src/rope_bridge.rs
+++ days/day_9/src/rope_bridge.rs
@@ -27,17 +27,23 @@ impl RopeBridge {
         }
     }
 
-    pub fn unique_tail_locations(&self) -> i32 {
+    pub fn unique_tail_locations(&self, knot_count: i32) -> i32 {
         let mut unique_locations: HashSet<(i32, i32)> = HashSet::new();
-        let mut head_x = 0;
-        let mut head_y = 0;
-        let mut tail_x = 0;
-        let mut tail_y = 0;
+        let mut coords: Vec<(i32, i32)> = Vec::new();
+
+        for _ in 0..(knot_count + 1) {
+            coords.push((0, 0));
+        }
 
         for step in &self.steps {
             for _ in 0..step.count {
-                move_head(&mut head_x, &mut head_y, &step.direction);
-                maybe_move_tail(&mut head_x, &mut head_y, &mut tail_x, &mut tail_y);
+                move_head(&mut coords, &step.direction);
+
+                for index in 1..(coords.len()) {
+                    maybe_move_tail(&mut coords, index);
+                }
+
+                let (tail_x, tail_y) = coords[coords.len() - 1];
                 unique_locations.insert((tail_x, tail_y));
             }
         }
@@ -46,18 +52,23 @@ impl RopeBridge {
     }
 }
 
-fn move_head(head_x: &mut i32, head_y: &mut i32, direction: &Direction) {
+fn move_head(coords: &mut [(i32, i32)], direction: &Direction) {
+    let (x, y) = coords[0];
     match direction {
-        Direction::Up => *head_y += 1,
-        Direction::Down => *head_y -= 1,
-        Direction::Left => *head_x -= 1,
-        Direction::Right => *head_x += 1,
+        Direction::Up => coords[0] = (x, y + 1),
+        Direction::Down => coords[0] = (x, y - 1),
+        Direction::Left => coords[0] = (x - 1, y),
+        Direction::Right => coords[0] = (x + 1, y),
     }
 }
 
-fn maybe_move_tail(head_x: &mut i32, head_y: &mut i32, tail_x: &mut i32, tail_y: &mut i32) {
-    if tail_trailing(head_x, head_y, tail_x, tail_y) {
-        move_tail(head_x, head_y, tail_x, tail_y);
+fn maybe_move_tail(coords: &mut [(i32, i32)], index: usize) {
+    let (head_x, head_y) = coords[index - 1];
+    let (mut tail_x, mut tail_y) = coords[index];
+
+    if tail_trailing(&head_x, &head_y, &tail_x, &tail_y) {
+        move_tail(&head_x, &head_y, &mut tail_x, &mut tail_y);
+        coords[index] = (tail_x, tail_y);
     }
 }
 
@@ -65,7 +76,7 @@ fn tail_trailing(head_x: &i32, head_y: &i32, tail_x: &i32, tail_y: &i32) -> bool
     (tail_x - head_x).abs() > 1 || (tail_y - head_y).abs() > 1
 }
 
-fn move_tail(head_x: &mut i32, head_y: &mut i32, tail_x: &mut i32, tail_y: &mut i32) {
+fn move_tail(head_x: &i32, head_y: &i32, tail_x: &mut i32, tail_y: &mut i32) {
     if head_x == tail_x {
         move_coords(head_y, tail_y);
     } else if head_y == tail_y {
@@ -76,8 +87,8 @@ fn move_tail(head_x: &mut i32, head_y: &mut i32, tail_x: &mut i32, tail_y: &mut 
     }
 }
 
-fn move_coords(head: &mut i32, tail: &mut i32) {
-    match head.cmp(&tail) {
+fn move_coords(head: &i32, tail: &mut i32) {
+    match head.cmp(tail) {
         Ordering::Less => *tail -= 1,
         Ordering::Greater => *tail += 1,
         Ordering::Equal => {}
